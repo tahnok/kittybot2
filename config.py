e33bcb313148e8985723f7c208dbb41e503d1b16
Wesley <tahnok881@hotmail.com>
2011-07-14 11:02:35 -0400

    correct config.py

---
+++ config.py
@@ -0,0 +1,16 @@
+"""
+Fill out these fields to make kittybot2 work
+"""
+##IRC related things
+network = 'irc.freenode.net'
+port = 6667
+channels = ['#mctest']
+nick = 'kittybot2'
+name = 'secretly a goose'
+owner = ''
+freenode = True
+nickservpwd = ''
+
+##Developer Keys
+ipinfodbkey = ''
+googlekey = ''
